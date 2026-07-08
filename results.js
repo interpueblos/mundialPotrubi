99b5714d8f71ff02415d761f397f565d1e6f5f94
LKSOUTSOURCING\a.basurto <abasurto@gobtech.lksnext.com>
2026-07-08 08:39:34 +0200

    Result hasta suiza

--- results.js
+++ results.js
@@ -137,9 +137,9 @@ const RESULTS = {
         // Partido 94: W81 vs W82
         {match: 94, team1: "Estados Unidos",  team2: "Bélgica",   winner: "Bélgica"},
         // Partido 95: W86 vs W88
-        {match: 95, team1: "Argentina",      team2: "Egipto",    winner: ""},
+        {match: 95, team1: "Argentina",      team2: "Egipto",    winner: "Argentina"},
         // Partido 96: W85 vs W87
-        {match: 96, team1: "Suiza",          team2: "Colombia",  winner: ""}
+        {match: 96, team1: "Suiza",          team2: "Colombia",  winner: "Suiza"}
       ],
       quarterfinals: [],
       semifinals: [],
